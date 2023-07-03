154dda1b55fbe854d999a41f4e93888e97ea046b
antik999 <antikill-fc@yandex.ru>
2023-07-04 00:59:57 +0300

    fix: fix 1 part

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
-    es6: true,
-    node: true,
-    mocha: true,
+    es2021: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
   },
   rules: {
   },
